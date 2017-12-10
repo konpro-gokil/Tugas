0de75a9fddb5590d6bba987e1c1e90ee64e59949
EkaSilfiani <ekasilfiani78@gmail.com>
2017-12-10 14:47:30 +0700

    sak sak e

---
+++ modul5.py
@@ -0,0 +1,13 @@
+Nama=["William","Kate","Anderson","Jame","Mady"]
+print("a: ", Nama)
+Nama+=["Jones",]
+print("b: ", Nama)
+Nama[2]="Grace"
+print("c: ", Nama)
+member="thompson" in Nama
+print("d: ", member)
+print("e: ", sorted(Nama))
+for x in Nama:
+	print("f: ", x)
+Nama.clear()
+print("g: ", Nama)
